22dbc2366cf78ec0740f3bde2ec748fe84c09c14
Lizi Shvelidze <l.shvelidze09@gmail.com>
2024-10-24 16:34:48 +0400

    new

---
+++ Abstract/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Abstract
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Proxy/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Xml.Linq;
+
+namespace Proxy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Subject subject = new Proxy();
+            subject.Request();
+
+
+            RealSubject realSubject = new RealSubject();
+            realSubject.Request();
+        }
+    }
+
+    internal class Subject
+    {
+    }
+}
